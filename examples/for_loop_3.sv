b6d3f0008c18c8cc12bb547c6932a96e56846b52
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-04 12:48:05 +0530

    Create for_loop_3.sv

---
+++ examples/for_loop_3.sv
@@ -0,0 +1,50 @@
+/*module loop_test;
+  bit clk;
+  bit [3:0] counter;
+  
+  always #10 clk=~clk;
+  
+  initial
+    begin
+      
+     $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+      
+      for (int i = 0; i<16; i++)
+        begin
+          @(posedge clk);
+          counter = i;
+          $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+        end
+      
+      $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+      $finish;
+    end
+endmodule*/
+
+module loop_test;
+  bit clk;
+  bit [3:0] counter;
+  bit [3:0] i;
+  
+  always #10 clk=~clk;
+  
+  initial
+    begin
+      
+     $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+      
+      for (i=0; i<16; i++)
+        begin
+          @(posedge clk);
+          counter = i;
+	  if (i == 15)
+		break;
+          $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+        end
+      
+      $display("time = %0t |counter = %b | counter = %d", $time, counter, counter);
+      $finish;
+    end
+endmodule
+  
+  
