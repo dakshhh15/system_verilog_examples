efe53e95dfbcdf7d3765576886d8fade4e31af8b
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-12 13:06:00 +0530

    Create randomize_static_dynamic_array.sv

---
+++ examples/oop/randomize_static_dynamic_array.sv
@@ -0,0 +1,19 @@
+class packet;
+  rand byte array [4];
+  rand byte array2 [];
+
+  constraint a2 { array2.size() > 2; array2.size() < 10; }
+endclass
+
+module tb;
+  packet pkt = new ();
+  
+  initial
+    begin
+      for (int i = 0; i<8; i++)
+        begin
+          pkt.randomize();
+          $display("array = %p | array = %p", pkt.array, pkt.array2);
+        end
+    end
+endmodule
