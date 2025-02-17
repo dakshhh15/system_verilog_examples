256ea5db3ea88ceda6afa4f0662e3bf09a21a7a7
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-17 17:50:06 +0530

    Create generator.sv

---
+++ testbenches/full adder/generator.sv
@@ -0,0 +1,20 @@
+class generator;
+  
+  transaction trans;
+  mailbox gen2driv;
+  
+  function new (mailbox gen2driv);
+    this.gen2driv = gen2driv;
+  endfunction
+  
+  task main ();
+    repeat (1)
+      begin
+        trans = new ();
+        assert (trans.randomize());
+        trans.display("generator");
+        gen2driv.put(trans);
+      end
+  endtask
+  
+endclass
