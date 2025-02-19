52a67d5197c432de5d0783cbfc17a968da80da87
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-19 12:36:25 +0530

    Create scoreboard.sv

---
+++ testbenches/4 bit full adder with forever loop/scoreboard.sv
@@ -0,0 +1,25 @@
+class scoreboard;
+  
+  mailbox mon2sb;
+  int count2;
+  
+  function new (mailbox mon2sb);
+    this.mon2sb = mon2sb;
+  endfunction
+  
+  task main;
+    transaction trans = new ();
+    forever
+      begin
+        mon2sb.get(trans);
+        trans.display(" SCOREBOARD ");
+        if (((trans.a + trans.b + trans.cin) == {trans.cout, trans.sum}))
+          $display(" MATCH ");
+        else
+          $error(" MISMATCH ");
+        count2++;
+        
+      end
+  endtask
+  
+endclass
