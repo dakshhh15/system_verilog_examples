4c38129292baaefee703c9832c1291845128f2a1
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-25 14:38:54 +0530

    Create concurrent.sv

---
+++ examples/assertions/concurrent.sv
@@ -0,0 +1,34 @@
+module assertion;
+  
+  bit clk, a, b;
+  
+  always #5 clk = ~ clk;
+  
+  initial
+    begin
+      $dumpvars(0,assertion);
+      $dumpfile("dump.vcd");
+    end
+  
+  initial
+    begin
+      $monitor(" [%0t] clk = %0d | a = %0b | b = %0b ", $time, clk, a, b);
+      a = 0;
+      #15 a = 1;
+      #20 a = 0;
+      #30 a = 1;
+      
+    end
+  
+  initial
+    begin
+      b = 0;
+      #10 b = 1;
+      #20 b = 0;
+      #30 b = 1;
+      #10 $finish;
+    end
+  
+  assert property (@(posedge clk) (a && b));
+  
+endmodule    
